1ceae34fe43bfbba797378116c2f78fcd9359c72
Juan Vargas <vargasjuan91@gmail.com>
2021-12-07 14:53:35 -0500

    Unit tests class characterController are improved

--- Rest-World-Disney-Service/infrastructure/entry-points/api-rest/src/test/java/co/com/disney/api/CharacterControllerTest.java
+++ Rest-World-Disney-Service/infrastructure/entry-points/api-rest/src/test/java/co/com/disney/api/CharacterControllerTest.java
@@ -1,21 +1,26 @@
 package co.com.disney.api;
 
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
 
+import java.util.ArrayList;
 import java.util.List;
-
+import java.util.Set;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.springframework.http.ResponseEntity;
 
 import co.com.disney.model.dto.request.CharacterRqDTO;
 import co.com.disney.model.dto.response.CharacterDTONameAndImage;
 import co.com.disney.model.dto.response.CharacterRsDTO;
+import co.com.disney.model.dto.response.MovieRsDetailsDTO;
 
 
 public class CharacterControllerTest {
@@ -26,17 +31,39 @@ public class CharacterControllerTest {
 	@Mock
 	private co.com.disney.usecase.disney.CharacterUseCase characterUseCase;
 	
+	private static Long ID = 1L;
+	
+	private Set<Long> movies;
+	
+	private List<MovieRsDetailsDTO> listMovie = new ArrayList<>();
+	
 	private CharacterRqDTO characterRqDTO;
 	
 	private String history = "Jasmine is a beautiful fifteen-year-old princess daughter of the Sultan of Agrabah. Unwilling to marry any of the princes her father suggests, she runs away from home, but because she has never been outside the palace before.";
 	
+	private List<CharacterDTONameAndImage> listCharacterDTONameAndImage = new ArrayList<>();
+	
+	private List<CharacterRsDTO> listCharacterRsDTO = new ArrayList<>();
+	
+	private CharacterDTONameAndImage characterDTONameAndImage;
+	
+	private CharacterRsDTO characterRsDTO;
 	
 	@Before
 	public void init () {
 		MockitoAnnotations.initMocks(this);
 		characterRqDTO = CharacterRqDTO.builder().name("Jasmine").age(18).history(history).image("/src/disney/image/jasmine.jpg").weight(54).build();
-		CharacterDTONameAndImage.builder().name("Jasmine").image("/src/disney/image/jasmine.jpg").build();
+		characterDTONameAndImage = CharacterDTONameAndImage.builder().name("Jasmine").image("/src/disney/image/jasmine.jpg").build();
+		listCharacterDTONameAndImage.add(characterDTONameAndImage);
+		characterRsDTO = CharacterRsDTO.builder().age(18).idCharacter(ID).image("/src/disney/image/jasmine.jpg").name("Noel").weight(54).listMovie(listMovie).build();	
+		listCharacterRsDTO.add(characterRsDTO);
 		when(characterUseCase.saveCharacter(characterRqDTO)).thenReturn(characterRqDTO);
+		when(characterUseCase.getListCharacterNameAndImage()).thenReturn(listCharacterDTONameAndImage);
+		when(characterUseCase.getAllCharacter()).thenReturn(listCharacterRsDTO);
+		when(characterUseCase.findCharacter(ID)).thenReturn(characterRsDTO);
+		when(characterUseCase.getCharactersByFilters("Noel", 18, movies, "ASC")).thenReturn(listCharacterRsDTO);
+		when(characterUseCase.updateCharacter(ID, characterRqDTO)).thenReturn(characterRqDTO);
+
 	}
 
 	
@@ -46,6 +73,8 @@ public class CharacterControllerTest {
 		ResponseEntity<CharacterRqDTO> response;
 		response = characterController.insert(characterRqDTO);
 		assertNotNull(response);
+		assertEquals("Jasmine", response.getBody().getName());
+		System.out.println(response.getBody().getName());
 
 	}
 	
@@ -56,6 +85,8 @@ public class CharacterControllerTest {
 		ResponseEntity<List<CharacterDTONameAndImage>> response;
 		response = characterController.listCharactersNameAndImage();
 		assertNotNull(response);
+		assertFalse(response.getBody().isEmpty()); //Return false if contains some object
+		System.out.println(response.getBody().isEmpty());
 
 	}
 	
@@ -65,6 +96,8 @@ public class CharacterControllerTest {
 		ResponseEntity<List<CharacterRsDTO>> response;
 		response = characterController.getAll();
 		assertNotNull(response);
+		assertFalse(response.getBody().isEmpty()); //Return false if contains some object
+		System.out.println(response.getBody().isEmpty());
 
 	}
 	
@@ -72,9 +105,10 @@ public class CharacterControllerTest {
 	public void testFindCharacterforID () {
 		
 		ResponseEntity<CharacterRsDTO> response;
-		Long id = 1L;
-		response = characterController.findCharacterforID(id);
+		response = characterController.findCharacterforID(ID);
 		assertNotNull(response);
+		assertEquals("Noel", response.getBody().getName());
+		System.out.println( response.getBody().getName());
 
 	}
 	
@@ -82,8 +116,9 @@ public class CharacterControllerTest {
 	public void testGetCharacterDetailsByFilters () {
 		
 		ResponseEntity<List<CharacterRsDTO>> response;
-		response = characterController.getCharacterDetailsByFilters("Jasmine", 18, null, "ASC");
+		response = characterController.getCharacterDetailsByFilters("Noel", 18, movies, "ASC");
 		assertNotNull(response);
+		assertFalse(response.getBody().isEmpty()); //Return false if contains some object
 
 	}
 	
@@ -91,19 +126,23 @@ public class CharacterControllerTest {
 	public void testUpdate () {
 		
 		ResponseEntity<CharacterRqDTO> response;
-		Long id = 1L;
-		response = characterController.update(id,characterRqDTO);
+		response = characterController.update(ID,characterRqDTO);
 		assertNotNull(response);
+		assertEquals("Jasmine", response.getBody().getName());
+		System.out.println("*************");
+		System.out.println(response.getBody().getName());
 
 	}
 	
 	@Test
 	public void testDelete () {
-		
+		Mockito.when(characterUseCase.deleteCharacter(ID)).thenReturn("Delete User");
 		ResponseEntity<String> response;
-		Long id = 1L;
-		response = characterController.delete(id);
+		response = characterController.delete(ID);
 		assertNotNull(response);
+		System.out.println("*************");
+		System.out.println(response.getBody());
+
 
 	}
 	
